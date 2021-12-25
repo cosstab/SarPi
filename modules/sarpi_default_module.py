93989ed636f94cd5df98a1d5ffc80546c51530c7
cosstab <alvarocosstab@gmail.com>
2021-12-25 02:25:11 +0100

    Dynamically list available commands

--- modules/sarpi_default_module.py
+++ modules/sarpi_default_module.py
@@ -7,12 +7,33 @@ class SarpiDefaultModule(SarpiModule):
 
     # Dictionary of commands and static responses
     __responses = {
-        "hello": "🤖 Hello and welcome to the 'PI' Automated Response Service, SarPI for short.\nYou can check available commands with .list\n\n🌍Web: http://sarpi.chabal.es",
-        "list": "List of available commands: hello, list, ping"
+        "hello": "🤖 Hello and welcome to the 'PI' Automated Response Service, SarPI for short.\nYou can check available commands with 'list'\n\n🌍Web: http://sarpi.chabal.es",
+        "ping": "Pong! 🏓",
+        "list": ""
     }
     
     COMMAND_WORDS = __responses.keys()
 
+    def get_commands(self):
+        command_list = []
+        response = ""
+
+        # Get commands from every installed module
+        for cls in self.modules:
+            command_list += cls.COMMAND_WORDS
+        
+        command_list.sort()
+        
+        # Format response
+        for command in command_list:
+            response += "·" + command + "\n"
+
+        return response
+
     def process_message(self, message: SarpiMessage):
-        response = self.__responses.get(message.command) #Choose the appropiate command response
+        if (message.command == "list"):
+            response = "List of available commands:\n" + self.get_commands() #List available commands
+        else:
+            response = self.__responses.get(message.command) #Choose the appropiate command response
+
         message.medium.reply(SarpiMessage(response))
